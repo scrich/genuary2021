1256d2bd113e40cc809f2e0f33c0ec98a882768d
scrich <rich@scrich.co.uk>
2021-01-29 17:47:06 +0000

    sound discovery

--- 20210128/sketch.js
+++ 20210128/sketch.js
@@ -1,14 +1,62 @@
 const DEBUG = false;
 
-
+var wave;
+// var context  = new AudioContext();
+let button;
+let playing;
+let env;
 
 function setup() {
-  let can = createCanvas(400, 400);
+  let can = createCanvas(100, 100);
   can.parent("canvas");
-  
+  playing = true;
+
+  button = createButton("play/pause");
+  button.mousePressed(playpause);
+
+  env = new p5.Env();
+  env.setADSR(1,2,0.3,3);
+  env.setRange(0.8,0);
+
+
+  wave = new p5.Oscillator();
+  wave.setType('sawtooth');
+  wave.start();
+  wave.amp(env);
+  wave.freq(220);
+
+
+
+
+}
+
+function playpause() {
+  env.play();
+  // console.log("playpause");
+  // console.log(playing);
+  // if (!playing) {
+  //   console.log(playing);
+  //   wave.amp(0.25,1);
+  //   playing = true;
+  // } else {
+  //   console.log(playing);
+  //   playing = false;
+  //   wave.amp(0,1);
+  //   console.log(playing);
+  // }
 }
 
 function draw() {
-  background(220);  
+  if (playing) {
+    background(color("black"));
+  } else {
+    background(color("red"));
+  }
+  wave.freq(mouseY);
+  textAlign(CENTER, CENTER);
+  text(getAudioContext().state, width / 2, height / 2);
+}
 
+function mousePressed() {
+  userStartAudio();
 }
